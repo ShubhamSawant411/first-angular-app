8d8952ef0db669361c5e538d9e43f7f58fe95096
ShubhamSawant411 <shubhamsawant@nimapinfotech.com>
2024-06-05 11:53:35 +0530

    first commit

---
+++ src/app/binding/binding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-binding',
+  templateUrl: './binding.component.html',
+  styleUrl: './binding.component.css'
+})
+export class BindingComponent {
+  interPolation:string = "https://bs-uploads.toptal.io/blackfish-uploads/components/seo/5911499/og_image/optimized/top-18-most-common-angularjs-developer-mistakes-41f9ad303a51db70e4a5204e101e7414.png";
+  imageUrl:string=""
+}
